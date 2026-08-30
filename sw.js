6fa366be39a03b7df0998f45eb3548cf017757a7
Jaden <sjm8024@naver.com>
2026-08-30 19:16:20 +0900

    Refine cluster marker color and size

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "parkview-v86";
+const CACHE_NAME = "parkview-v87";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=86",
+  "./styles.css?v=87",
   "./config.js?v=2",
   "./app.js?v=85",
   "./manifest.webmanifest"
